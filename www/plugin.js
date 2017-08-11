255fe101d7a010af733ac01f7831b6d0a6e7c252
mibzman <sam@hungerperks.com>
2017-08-11 09:39:48 -0400

    Added test

--- www/plugin.js
+++ www/plugin.js
@@ -5,7 +5,7 @@ var PLUGIN_NAME = 'MyCordovaPlugin';
 
 var MyCordovaPlugin = {
   echo: function(phrase, cb) {
-    exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
+    exec(cb, null, PLUGIN_NAME, 'echo', [phrase + "this has been echoed by a plugin"]);
   },
   getDate: function(cb) {
     exec(cb, null, PLUGIN_NAME, 'getDate', []);
