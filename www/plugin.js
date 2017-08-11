1e89175b0fee2a117d31c807296744d9eb2181ca
mibzman <sam@hungerperks.com>
2017-08-11 09:28:44 -0400

    Changed name back

--- www/plugin.js
+++ www/plugin.js
@@ -1,9 +1,9 @@
 
 var exec = require('cordova/exec');
 
-var PLUGIN_NAME = 'DiyCordovaPlugin';
+var PLUGIN_NAME = 'MyCordovaPlugin';
 
-var DiyCordovaPlugin = {
+var MyCordovaPlugin = {
   echo: function(phrase, cb) {
     exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
   },
@@ -12,4 +12,4 @@ var DiyCordovaPlugin = {
   }
 };
 
-module.exports = DiyCordovaPlugin;
+module.exports = MyCordovaPlugin;
